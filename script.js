025aaeca127cff117b105134dad4565a660b10c0
dewa0702 <dewangga.putra1592@smk.belajar.id>
2025-06-01 21:20:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+    VanillaTilt.init(document.querySelectorAll('.tourist-image img'), {
+            max: 20,
+            speed: 400,
+            glare: true,
+            "max-glare": 0.3,
+        });
